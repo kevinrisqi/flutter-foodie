f68595e7a198b77b787df3a9cdaf0cb80959576e
fanifadhil <fanifadhil640@gmail.com>
2022-09-05 14:34:33 +0700

    Create constant theme

---
+++ lib/config/themes/theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color blackColor = const Color(0xff2E2E2E);
+Color fullBlackColor = const Color(0xff1E1E1E);
+Color greyColor = const Color(0xff8F8F8F);
+Color primaryColor = const Color(0xff00717F);
+Color backgroundColor = const Color(0xffFFFFFF);
+Color alertColor = const Color(0xffD81D1D);
+
+TextStyle primaryTextStyle = GoogleFonts.montserrat(color: fullBlackColor);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
